db57a83529b1544fcbdd6138e7e8f001cd24c6f0
M.Ali Koken <makoken@gmail.com>
2022-09-08 21:58:54 +0200

    another run

--- build/Tasks/DockerPushTask.cs
+++ build/Tasks/DockerPushTask.cs
@@ -22,13 +22,13 @@ public sealed class DockerEcrPushTask : FrostingTask<BuildContext>
                 if (dockerSetting.RunDockerPush == true)
                 {
                     var awsApp = context.Config.AwsApplication;
-                    context.DockerEcrLogin(dockerSetting.AwsAccountNumber,dockerSetting.AwsRegion);
                     var buildTagName = context.Config.AwsApplication.GetResourceName(dockerSetting.DockerRepoNameSuffix);
                     var ecrTagNameBase = $"{dockerSetting.AwsAccountNumber}.dkr.ecr.{dockerSetting.AwsRegion}.amazonaws.com/{buildTagName}";
                     var ecrTagNameVersion = $"{ecrTagNameBase}:{context.Config.AwsApplication.Version}";
                     var ecrTagNameLatest = $"{ecrTagNameBase}:{context.Config.AwsApplication.Version}";
                     context.DockerTag(buildTagName,ecrTagNameVersion);
                     context.DockerTag(buildTagName,ecrTagNameLatest);
+                    context.DockerEcrLogin(dockerSetting.AwsAccountNumber,dockerSetting.AwsRegion);
                     context.DockerPush(ecrTagNameVersion);
                     context.DockerPush(ecrTagNameLatest);
                 }
